ec538174ff8a3b1ae719ba54353d64f7706655e9
Arnav Shekaran <arnard76@gmail.com>
2023-04-20 01:12:43 +1200

    show page name on each page & in the title 😃

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<svelte:head>
+  <title>
+    {$page.route.id.replaceAll("/", " ").trim()}
+  </title>
+</svelte:head>
+
+<p>
+  {$page.route.id.replaceAll("/", " ").trim()}
+</p>
+<slot />
+
+<style>
+  p {
+    font-weight: 900;
+    font-size: 20px;
+    text-transform: uppercase;
+  }
+</style>
