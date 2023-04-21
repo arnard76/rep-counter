ffe526d9eda583dcd89fb32d96333eb456177e45
Arnav Shekaran <arnard76@gmail.com>
2023-04-21 22:19:46 +1200

    home page attempt #1
    working links
    
    but no menu-like structure finished yet

---
+++ src/lib/site-nav/Menu.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let structuredPages = [];
+
+  let menuStyles = "display:flex; background-color:rgb(150, 150, 150); ";
+  let verticalStyles = "flex-direction: column; background-color:lightgrey;";
+
+  function createMenu(structuredPage, horizontal = true) {
+    let menu = `<li style="${menuStyles} ${
+      !horizontal ? verticalStyles : ""
+    }"><p><a href="${structuredPage.url}"> ${structuredPage.name}</a></p>
+      `;
+
+    console.log(structuredPage);
+
+    for (let page of structuredPage.subPages) {
+      //   console.log(page);
+      menu = menu + createMenu(page, !horizontal);
+    }
+    return menu + "</li>";
+  }
+</script>
+
+{#each structuredPages as structuredPage (structuredPage.name)}
+  <ul>
+    {@html createMenu(structuredPage)}
+  </ul>
+{/each}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,93 @@
+<script>
+  import Menu from "$lib/site-nav/Menu.svelte";
+  import { browser } from "$app/environment";
+
+  let allPages = Object.keys(import.meta.glob("./**/**.svelte"));
+  let usefulPages = allPages
+    .filter((possiblePage) => {
+      if (possiblePage.includes("+layout.svelte")) return;
+      if (!possiblePage.includes("+page.svelte")) return;
+      return true;
+    })
+    .map((usefulPage) => {
+      let cleanedPageName = usefulPage;
+
+      cleanedPageName = cleanedPageName
+        .replace("+page.svelte", "")
+        .replace("./", "")
+        .replaceAll("/", "|");
+
+      cleanedPageName = cleanedPageName.substring(
+        0,
+        cleanedPageName.length - 1
+      );
+
+      console.log("ting", cleanedPageName);
+      return {
+        url: usefulPage.replace("+page.svelte", ""),
+        pageTitle: cleanedPageName,
+      };
+    });
+
+  // turn into class?
+  let structuredMenu = [];
+
+  /**
+   * adds a new page into the structured menu
+   * @param {Array} structuredMenu
+   * @param {Array} pageToAdd - includes a list of sub routes of the page
+   */
+  function addAPage(structuredMenu, pageToAdd) {
+    let existingStructuredPage = structuredMenu.findIndex((structuredPage) => {
+      console.log(structuredPage);
+      return structuredPage.name === pageToAdd[0];
+    });
+    console.log(existingStructuredPage, pageToAdd);
+
+    if (existingStructuredPage !== -1) {
+      let existingRoute = pageToAdd.splice(0, 1)[0];
+      console.log("existing", existingStructuredPage, existingRoute);
+      structuredMenu[existingStructuredPage].subPages = addAPage(
+        structuredMenu[existingStructuredPage].subPages,
+        pageToAdd
+      );
+    } else {
+      let remainingSubRoutes = pageToAdd.slice(1);
+      let subPages = !remainingSubRoutes.length
+        ? []
+        : addAPage([], remainingSubRoutes);
+
+      structuredMenu.push({
+        name: pageToAdd[0],
+        subPages,
+      });
+    }
+    return structuredMenu;
+  }
+
+  $: if (usefulPages) {
+    for (let usefulPage of usefulPages) {
+      let subRoutes = usefulPage.pageTitle.split("|");
+      //   subRoutes.pop();
+      //   let lastRoute = subRoutes.pop();
+      //   console.log(usefulPage, subRoutes, lastRoute);
+
+      structuredMenu = addAPage(structuredMenu, subRoutes);
+      //   console.log(structuredMenu);
+    }
+  }
+
+  //   if (browser) {
+  //     structuredMenu = addAPage(structuredMenu, ["cool", "beans"]);
+  //   }
+  $: console.log(structuredMenu);
+</script>
+
+{#if usefulPages}
+  {#each usefulPages as { url, pageTitle } (url)}
+    <p>
+      <a href={url}>{pageTitle}</a>
+    </p>
+  {/each}
+  <!-- <Menu structuredPages={structuredMenu} /> -->
+{/if}
