d12d50589e2e8b4cb00642cdd79ae57e0a102d8f
Arnav Shekaran <arnard76@gmail.com>
2023-04-21 22:04:57 +1200

    more concise input structure for SelectOne
    using select instead of radio inputs

--- src/lib/inputs/SelectOneKeypoint.svelte
+++ src/lib/inputs/SelectOneKeypoint.svelte
@@ -2,24 +2,15 @@
   import { getVerboseKeypointName } from "$lib/transformKeypointData.js";
 
   export let keypointNames;
-
   export let selectedKeypoint = null;
 </script>
 
 {#if keypointNames}
-  <div
-    style="display:grid; max-width: 100vw; grid-template-columns: repeat(6, 100px);"
-  >
+  <select bind:value={selectedKeypoint}>
     {#each keypointNames as keypointName (keypointName)}
-      <div style="display:flex; margin-inline: 50px;">
-        <p>{getVerboseKeypointName(keypointName)}</p>
-
-        <input
-          type="radio"
-          bind:group={selectedKeypoint}
-          value={keypointName}
-        />
-      </div>
+      <option value={keypointName}
+        >{getVerboseKeypointName(keypointName)}</option
+      >
     {/each}
-  </div>
+  </select>
 {/if}
