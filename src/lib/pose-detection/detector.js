ca1dc63454dafdf68a2308af2377de4e7e84a9e9
Arnav Shekaran <arnard76@gmail.com>
2023-05-06 02:25:39 +1200

    bug🐜: web gpu wasn't ready but a backend is required for creating a detector
    web gpu is one of the possible backends in tfjs:
    1. cpu
    2. webgl
    3. webgpu
    
    4. other ones for node.js
    
    this only happened in brave not edge or chrome. WHY?
    - Brave updated to chromium 113 which supports WebGPU. 🦁🦁
    - Chrome using chromium 112 🤡
    - Edge using chromium 112 🤡
    🤯🤯🤯🤯🤯
    
    Why wasn't WebGPU ready in backend?
    - WebGPU requires some time to be ready ... 🙄
    https://www.npmjs.com/package/@tensorflow/tfjs-backend-webpu
    - unlike WebGL 🤦
    https://www.npmjs.com/package/@tensorflow/tfjs-backend-webgl
    
    THE FIX! 🔨🛠️🪛👷‍♂️:
    wait for the backend to be ready using built-in function.
    
    alternatives:
    could also set backend manually and wait for that too. E.g. tf.setBackend('webgpu')
    
    or just set to webgl always as that doesn't have a wait time for some reason
    tf.setBackend('webgl')

--- src/lib/pose-detection/detector.js
+++ src/lib/pose-detection/detector.js
@@ -1,11 +1,13 @@
+import * as tf from "@tensorflow/tfjs-core";
 import * as poseDetection from "@tensorflow-models/pose-detection";
 import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl";
 
 async function createDetector() {
+  await tf.ready();
+
   const model = poseDetection.SupportedModels.MoveNet;
   const modelType = poseDetection.movenet.modelType.SINGLEPOSE_THUNDER;
-
   return poseDetection.createDetector(model, { modelType });
 }
 
