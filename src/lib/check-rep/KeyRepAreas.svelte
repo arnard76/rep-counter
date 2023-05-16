033db1ab03ba946e582a107abf0cade5111a88c5
Arnav Shekaran <arnard76@gmail.com>
2023-05-17 01:27:41 +1200

    create a component to display many key rep areas 😀😀

---
+++ src/lib/check-rep/KeyRepAreas.svelte
@@ -0,0 +1,11 @@
+<script>
+  import KeyRepArea from "$lib/check-rep/KeyRepArea.svelte";
+
+  export let keyRepAreas;
+  export let keypoints;
+  export let focusKeypoint;
+</script>
+
+{#each keyRepAreas as keyRepArea (keyRepArea)}
+  <KeyRepArea {keyRepArea} {keypoints} {focusKeypoint} />
+{/each}
