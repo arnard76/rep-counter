7fa6d56085cf937395dffe7f710ef3716448812a
Arnav Shekaran <arnard76@gmail.com>
2023-06-06 23:37:30 +1200

    a group of keypoints can have a colour theme 🏳️‍🌈🌈🌈😃😃😀
    completely optional 🦥

--- src/lib/common-shapes/KeypointsOverlay.svelte
+++ src/lib/common-shapes/KeypointsOverlay.svelte
@@ -2,12 +2,13 @@
   import Keypoint from "$lib/common-shapes/Keypoint.svelte";
 
   export let keypoints;
+  export let colourTheme = null;
 </script>
 
 <div id="diagram" style="position:absolute;">
   {#if keypoints}
     {#each keypoints as keypoint (keypoint)}
-      <Keypoint {keypoint} />
+      <Keypoint {keypoint} colour={colourTheme} />
     {/each}
   {/if}
 </div>
