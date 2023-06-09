0c2fa594140b6f3acc39f481372c340025222554
Arnav Shekaran <arnard76@gmail.com>
2023-06-09 15:10:28 +1200

    create key rep areas store 🗃️🗃️
    stores as KeyRepArea instances not data.
    
    Whenever database is updated, all key rep areas data (in JS object form) gets turned into KRA instances.
    
    Now also create stores for all focus keypoints and all relative to keypoints. So I can highlight them on UI. Makes it easier to visualise key rep areas 😃😃
    
    a list to see, delete and update key rep areas.
    
    NOTE: Only updates LOCALLY!! Not in the database. That will happen in next few commits.

---
+++ src/lib/key-rep-area/CRUD/KRAsList.svelte
@@ -0,0 +1,53 @@
+<script>
+  import exercise from "$lib/key-rep-area/keyRepAreas.js";
+  import SelectOneKeypointName from "$lib/inputs/SelectOneKeypoint.svelte";
+  import keypointNames from "$lib/pose-detection/keypointNames.json";
+</script>
+
+<div style="height:100%; overflow-y:scroll;">
+  {#if $exercise}
+    {#each Object.entries($exercise.keyRepAreas) as [focusLimbName, keyRepAreasForLimb] (focusLimbName)}
+      <div class="KRAs-for-limb">
+        <p>{focusLimbName}</p>
+
+        {#each keyRepAreasForLimb as keyRepArea, index (keyRepArea)}
+          {@const { areaSize, topLeft } = keyRepArea}
+          <div class="key-rep-area">
+            <p>{areaSize.width}x{areaSize.height}</p>
+            <p>({topLeft.x}, {topLeft.y})</p>
+
+            <SelectOneKeypointName
+              {keypointNames}
+              bind:selectedKeypoint={$exercise.keyRepAreas[focusLimbName][index]
+                .relativeToWhichKeypoint}
+            />
+
+            <button
+              type="button"
+              on:click={() => {
+                exercise.deleteKRA(focusLimbName, keyRepArea);
+              }}>delete🗑️</button
+            >
+          </div>
+        {/each}
+      </div>
+    {/each}
+  {/if}
+</div>
+
+<style>
+  .KRAs-for-limb {
+    background-color: rgba(0, 25, 35, 0.15);
+    margin-top: 20px;
+  }
+
+  .key-rep-area {
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+  }
+
+  .key-rep-area > p {
+    width: 55%;
+  }
+</style>
---
+++ src/lib/key-rep-area/keyRepAreas.js
@@ -0,0 +1,88 @@
+import { writable } from "svelte/store";
+import KeyRepArea from "$lib/key-rep-area/keyRepArea.js";
+
+const keyRepAreas = createKRAsStore();
+
+function createKRAsStore() {
+  const store = writable(null);
+
+  return {
+    subscribe: store.subscribe,
+    set: store.set,
+
+    syncWithDatabase(newKRAsData) {
+      store.set(turnKRAsDataToKRAInstances(newKRAsData));
+    },
+
+    /**
+     * Adds KRA locally
+     * NOTE: have to save in order to put in DB
+     * @param {string} focusLimbName
+     * @param {KeyRepArea} KRA
+     */
+    addKRA(focusLimbName, KRA) {
+      if (!focusLimbName || !(typeof focusLimbName === "string")) {
+        console.error(
+          `this is not a valid focusLimbName (${focusLimbName}) as it is not a string (has type ${typeof focusLimbName})`
+        );
+        return;
+      }
+
+      if (!(KRA instanceof KeyRepArea)) {
+        console.error(
+          `this key rep area (${KRA}) is not of type KeyRepArea. It is type ${typeof keyArea}.`
+        );
+        return;
+      }
+
+      store.update(($exercise) => {
+        if (!Object.keys($exercise.keyRepAreas).includes(focusLimbName)) {
+          $exercise.keyRepAreas[focusLimbName] = [];
+        }
+        $exercise.keyRepAreas[focusLimbName].push(KRA);
+        return $exercise;
+      });
+    },
+
+    deleteKRA(focusLimbName, KRA) {
+      store.update((currentKRAs) => {
+        currentKRAs.keyRepAreas[focusLimbName].splice(
+          currentKRAs.keyRepAreas[focusLimbName].indexOf(KRA),
+          1
+        );
+
+        if (!currentKRAs.keyRepAreas[focusLimbName].length) {
+          delete currentKRAs.keyRepAreas[focusLimbName];
+        }
+
+        return currentKRAs;
+      });
+    },
+  };
+}
+
+function turnKRAsDataToKRAInstances(KRAsData) {
+  if (!KRAsData) return;
+
+  let keyRepAreas = {};
+  let keyRepAreasForLimb;
+  for (let [focusLimbName, keyRepAreasForLimbObject] of Object.entries(
+    KRAsData.keyRepAreas
+  )) {
+    keyRepAreasForLimb = [];
+    for (let {
+      areaSize,
+      topLeft,
+      relativeToWhichKeypoint,
+    } of keyRepAreasForLimbObject) {
+      keyRepAreasForLimb.push(
+        new KeyRepArea(relativeToWhichKeypoint, topLeft, areaSize)
+      );
+    }
+    keyRepAreas[focusLimbName] = keyRepAreasForLimb;
+  }
+
+  return { exerciseName: KRAsData.exerciseName, keyRepAreas };
+}
+
+export default keyRepAreas;
