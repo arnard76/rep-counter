ff1d2959b299f42d2a8bcbb52bd35ba2725dda35
Arnav Shekaran <arnard76@gmail.com>
2023-06-29 19:20:38 +1200

    adding exercise from exercise list ➕🎽🏃‍♀️😃😎😎

--- src/routes/multiple-exercises/+page.svelte
+++ src/routes/multiple-exercises/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import { createExercise } from "$lib/exercises/CRUD/crudDB";
   import exercises from "$lib/exercises/store";
+
+  const defaultExerciseFormat = {
+    name: "untitled",
+    focusLimbs: {},
+  };
 </script>
 
 {#if $exercises}
@@ -13,5 +19,11 @@
         </a>
       </li>
     {/each}
+
+    <li>
+      <button on:click={() => createExercise({ ...defaultExerciseFormat })}
+        >➕ new exercise</button
+      >
+    </li>
   </ul>
 {/if}
