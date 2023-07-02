4d06565fde225d4f12cc7e03fedd23af204bc529
Arnav Shekaran <arnard76@gmail.com>
2023-07-02 18:40:50 +1200

    refactoring gets rid of warning of "<InputVideoFile> was created with unknown prop ''validVideoFile"
    using an update function which is passed from parent to child
    
    update function can be changed in parent without error & that would probs not be the best lol

--- src/lib/video/InputVideoFile.svelte
+++ src/lib/video/InputVideoFile.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
+  import { onDestroy } from "svelte";
   import { writable } from "svelte/store";
 
   // grabbing the video file via html input
   let inputtedVideoFiles: FileList;
-  const validVideoFileSTRONG = writable(null);
-  export const validVideoFile = { subscribe: validVideoFileSTRONG.subscribe };
+  export let updateVideoFile: (videoFile: File) => void;
   const acceptedVideoFileExtensions = ["mp4", "mpeg", "avi"];
+
+  const updateVid = (vid) => {
+    updateVideoFile(vid);
+  };
 
   $: if (inputtedVideoFiles?.length) {
     let filename = inputtedVideoFiles[0].name.split(".");
     let fileExtensions = filename[filename.length - 1];
 
     if (acceptedVideoFileExtensions.includes(fileExtensions)) {
-      $validVideoFileSTRONG = inputtedVideoFiles[0];
+      updateVid(inputtedVideoFiles[0]);
     }
   }
+
+  onDestroy(() => updateVid(null));
 </script>
 
 <input
