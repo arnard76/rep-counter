09c83459b483ab041fe30f18d874fa635e1f3e55
Arnav Shekaran <arnard76@gmail.com>
2023-07-11 18:07:34 +1200

    adding single-page & testing pages 😳😲🥹🥹
    new database functions to facilitate managing exercises via app instead of on firebase console 👍👍
    
    debug button so can conveniently pause in dev tools while UI is paused as well 👍😎😎

--- src/lib/exercises/CRUD/crudDB.ts
+++ src/lib/exercises/CRUD/crudDB.ts
@@ -1,5 +1,5 @@
 import { database } from "$lib/exercises/CRUD/firebase.js";
-import { ref, set, onValue, push } from "firebase/database";
+import { ref, set, onValue, push, remove } from "firebase/database";
 import exercises from "$lib/exercises/store.js";
 import type { ExercisesData, ExerciseData } from "$lib/exercises";
 
@@ -11,6 +11,17 @@ function createExercise(exercise: ExerciseData) {
   push(ref(database, `development-db/`), exercise);
 }
 
+function duplicateExercise(exercise: ExerciseData) {
+  if (!exercise) return;
+  push(ref(database, `development-db/`), exercise);
+}
+
+function deleteExercise(exerciseId: string) {
+  if (!exerciseId) return;
+
+  remove(ref(database, `development-db/${exerciseId}`));
+}
+
 function streamExercises() {
   return onValue(ref(database, `development-db/`), (snapshot) => {
     const exerciseData: ExercisesData = snapshot.val();
@@ -19,4 +30,10 @@ function streamExercises() {
   });
 }
 
-export { streamExercises, createExercise, updateExercise };
+export {
+  streamExercises,
+  createExercise,
+  duplicateExercise,
+  updateExercise,
+  deleteExercise,
+};
