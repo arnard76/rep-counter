051bb5785bc10d03799fa1e7528585518843e2d2
Arnav Shekaran <arnard76@gmail.com>
2023-07-24 22:38:44 +1200

    fix bad import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Menu from "$legacy-pages-lib/site-nav/Menu.svelte";
+  import Menu from "$lib/site-nav/Menu.svelte";
   import { browser } from "$app/environment";
   let structuredPages = [];
   try {
