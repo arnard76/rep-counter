b70ed34d90068ebdd2853ee75dc138874a64e9ae
Arnav Shekaran <78939786+arnard76@users.noreply.github.com>
2023-07-27 23:07:05 +1200

    simplified site for users' sake

---
+++ src/lib/site-nav/Header.svelte
@@ -0,0 +1,19 @@
+<div>
+  <p>rep-counter</p>
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 0;
+    background-color: lightgreen;
+  }
+
+  p {
+    margin: 0;
+    font-weight: 900;
+    font-size: 20px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,84 +1,112 @@
-<script>
-  import Menu from "$lib/site-nav/Menu.svelte";
-  import { browser } from "$app/environment";
+<script lang="ts">
+  import Header from "$lib/site-nav/Header.svelte";
 
-  let allComponents = Object.keys(import.meta.glob("./**/**.svelte"));
-  let allPages = allComponents
-    .filter(
-      (possiblePage) =>
-        possiblePage.includes("+page.svelte") && !possiblePage.includes("[")
-    )
+  import exercises, {
+    selectedExerciseId,
+    selectedExercise,
+  } from "$lib/exercises/store";
 
-    .map((usefulPage) => {
-      let cleanedPageName = usefulPage;
+  import VideoFromFile from "$lib/video/VideoFromFile.svelte";
+  import LiveVideo from "$lib/video/LiveVideo.svelte";
+  import PauseControls from "$lib/paused/PauseControls.svelte";
 
-      cleanedPageName = cleanedPageName
-        .replace("/+page.svelte", "")
-        .replace("./", "")
-        .replaceAll("/", "|");
+  import { onDestroy } from "svelte";
+  import { scale } from "$lib/pose-detection/scaleKeypoints";
+  import {
+    focusLimbKeypoints,
+    relativeToKeypoints,
+  } from "$lib/pose-detection/otherKeypointStores";
+  import KeypointsOverlay from "$lib/pose-detection/KeypointsOverlay.svelte";
 
-      return {
-        url: usefulPage.replace("+page.svelte", ""),
-        pageTitle: cleanedPageName,
-      };
-    });
+  import KeyRepAreas from "$lib/key-rep-area/KeyRepAreas.svelte";
 
-  // turn into class?
-  let structuredPages = [];
+  import RepCounter from "$lib/count-reps/RepCounter.svelte";
+  import ExerciseSelector from "$lib/exercises/ExerciseSelector.svelte";
+  import VideoSourceToggle from "$lib/video/VideoSourceToggle.svelte";
 
-  /**
-   * adds a new page into the structured menu
-   * @param {Array} pagesInLevel
-   * @param {Array} pageToAdd - includes a list of sub routes of the page
-   * @param {string} pageUrl - relative link to page
-   */
-  function addPage(pagesInLevel, pageToAdd, pageUrl) {
-    if (pageToAdd.length === 0) return pagesInLevel;
-
-    let existingPageIndex = pagesInLevel.findIndex(
-      (page) => page.name == pageToAdd[0]
-    );
-
-    if (existingPageIndex < 0) {
-      return [
-        ...pagesInLevel,
-        {
-          name: pageToAdd[0],
-          url: pageUrl,
-          subPages: addPage([], pageToAdd.splice(1), pageUrl),
-        },
-      ];
-    }
-
-    pagesInLevel[existingPageIndex].subPages = addPage(
-      pagesInLevel[existingPageIndex].subPages,
-      pageToAdd.splice(1),
-      pageUrl
-    );
-    return pagesInLevel;
-  }
-
-  for (let { pageTitle, url } of allPages) {
-    let subRoutes = pageTitle.split("|");
-
-    structuredPages = addPage(structuredPages, subRoutes, url);
-  }
-
-  if (browser && "serviceWorker" in navigator) {
-    addEventListener("load", function () {
-      navigator.serviceWorker.register("service-worker.js");
+  let clientWidth: number,
+    clientHeight: number,
+    videoWidth: number,
+    videoHeight: number;
+  $: if (clientWidth && clientHeight && videoWidth && videoHeight) {
+    scale.set({
+      horizontal: clientWidth / videoWidth,
+      vertical: clientHeight / videoHeight,
     });
   }
+
+  onDestroy(() => {
+    scale.set({
+      horizontal: 1,
+      vertical: 1,
+    });
+  });
+
+  let live: boolean = false;
+  let validVideoFile: File | null;
 </script>
 
 <svelte:head>
   <title>rep-counter</title>
-  <meta
-    name="description"
-    content="Navigate to any page created during the development of rep-counter."
-  />
 </svelte:head>
 
-{#if structuredPages?.length}
-  <Menu pagesAtLevel={structuredPages} topLevel />
+<Header />
+<ExerciseSelector />
+<hr />
+<VideoSourceToggle bind:live bind:validVideoFile />
+<hr />
+
+{#if $selectedExercise && (live || validVideoFile)}
+  <div class="container" style="--height: {clientHeight || 150}px;">
+    <div class="video-container" bind:clientHeight bind:clientWidth>
+      {#if live}
+        <LiveVideo bind:videoWidth bind:videoHeight />
+      {:else}
+        <VideoFromFile bind:videoWidth bind:videoHeight {validVideoFile} />
+      {/if}
+
+      <RepCounter focusLimbs={$selectedExercise.focusLimbs} />
+
+      <!-- Relative for KRA's & Focus limb keypoints -->
+      <KeypointsOverlay keypoints={$relativeToKeypoints} colourTheme="gray" />
+      <KeypointsOverlay keypoints={$focusLimbKeypoints} colourTheme="gold" />
+
+      <!-- so mouse events can interact with 👇 but not ☝️ -->
+      <div class="divider" />
+
+      {#each Object.entries($selectedExercise.focusLimbs) as [focusLimb, { keyRepAreas }] (focusLimb)}
+        <KeyRepAreas
+          {keyRepAreas}
+          updateKeyRepAreas={(updated) =>
+            ($exercises[$selectedExerciseId].focusLimbs[focusLimb].keyRepAreas =
+              updated)}
+          focusKeypointName={focusLimb}
+        />
+      {/each}
+
+      <div style="position:absolute; bottom:0; left:0;">
+        <PauseControls />
+      </div>
+    </div>
+  </div>
 {/if}
+
+<style>
+  .container {
+    display: flex;
+    align-items: flex-start;
+    height: var(--height);
+    gap: 20px;
+  }
+
+  .video-container {
+    position: relative;
+  }
+
+  .divider {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    inset: 0;
+  }
+</style>
